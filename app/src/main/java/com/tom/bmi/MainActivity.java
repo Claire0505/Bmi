13aacd9218b7df98979dda0888644327a0f9c2da
Hank Tom <hanktom@gmail.com>
2018-01-20 22:23:53 +0800

    onclick屬性, LogCat

--- app/src/main/java/com/tom/bmi/MainActivity.java
+++ app/src/main/java/com/tom/bmi/MainActivity.java
@@ -2,6 +2,8 @@ package com.tom.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
+    }
+
+    public void bmi(View view){
+        String w = edWeight.getText().toString();
+        String h = edHeight.getText().toString();
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight/ (height * height);
+        Log.d("BMI", String.valueOf(bmi));
     }
 
     private void findViews() {
