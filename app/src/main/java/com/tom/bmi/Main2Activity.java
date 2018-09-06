c6eae3fa9aaecc18c5968a91bfd4e3f074e4a660
Claire <claire0505@gmail.com>
2018-09-07 00:54:44 +0800

    Activity 的生命週期

--- app/src/main/java/com/tom/bmi/Main2Activity.java
+++ app/src/main/java/com/tom/bmi/Main2Activity.java
@@ -19,6 +19,7 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
         setContentView(R.layout.activity_main2);
 
         findViews();
@@ -65,7 +66,41 @@ public class Main2Activity extends AppCompatActivity {
 
         startActivity(intent);
 
-
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestory", Toast.LENGTH_SHORT).show();
+    }
 }
